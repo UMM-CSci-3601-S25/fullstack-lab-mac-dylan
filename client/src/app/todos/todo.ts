4e6e297a9d8df5220b20fc63a2bf01f553c3022b
Dylan Packer <packe089@morris.umn.edu>
2025-02-18 19:46:29 -0600

    Added and updated the todo.service and todo.ts

---
+++ client/src/app/todos/todo.ts
@@ -0,0 +1,12 @@
+export interface Todo {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+isComplete: any;
+  _id: string;
+  owner: string;
+  body: string
+  status:boolean;
+  category: string;
+  avatar?: string;
+}
+
+export type TodoCategory = 'homework' | 'video games' | 'groceries' | 'software design';
