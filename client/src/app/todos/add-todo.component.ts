37d3843bd865072b5142fd695d7887c161847a9f
Dylan Packer <packe089@morris.umn.edu>
2025-02-18 19:48:11 -0600

    Made and created the add-todo part of the client side

---
+++ client/src/app/todos/add-todo.component.ts
@@ -0,0 +1,134 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { TodoService } from './todo.service';
+
+@Component({
+    selector: 'app-add-todo',
+    templateUrl: './add-todo.component.html',
+    styleUrls: ['./add-todo.component.scss'],
+    imports: [FormsModule, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatButtonModule]
+})
+export class AddTodoComponent {
+
+  addTodoForm = new FormGroup({
+    owner: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50),
+      (fc) => {
+        if (fc.value.toLowerCase() === 'abc123' || fc.value.toLowerCase() === '123abc') {
+          return ({existingName: true});
+        } else {
+          return null;
+      }
+    },
+    ])),
+
+    category: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50),
+    ])),
+
+    body: new FormControl('', Validators.compose([
+      Validators.required,
+      Validators.minLength(2),
+    ])),
+
+    status: new FormControl('incomplete', Validators.compose([
+      Validators.required,
+      Validators.pattern('^(complete|incomplete)$'),
+    ])),
+  });
+
+  readonly addTodoValidationMessages = {
+    owner: [
+      {type: 'required', message: 'Owner is required'},
+      {type: 'minlength', message: 'Owner must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Owner cannot be more than 50 characters long'},
+    ],
+
+    category: [
+      {type: 'required', message: 'Category is required'},
+      {type: 'minlength', message: 'Category must be at least 2 characters long'},
+      {type: 'maxlength', message: 'Category cannot be more than 50 characters long'},
+    ],
+
+    body: [
+      {type: 'required', message: 'Body is required'},
+      {type: 'minlength', message: 'Body must be at least 2 characters long'},
+    ],
+
+    status: [
+      { type: 'required', message: 'Status is required' },
+      { type: 'pattern', message: 'Status must be Complete or Incomplete' },
+    ]
+  };
+
+  constructor(
+    private todoService: TodoService,
+    private snackBar: MatSnackBar,
+    private router: Router) {
+  }
+
+  formControlHasError(controlName: string): boolean {
+    return this.addTodoForm.get(controlName).invalid &&
+      (this.addTodoForm.get(controlName).dirty || this.addTodoForm.get(controlName).touched);
+  }
+
+  getErrorMessage(name: keyof typeof this.addTodoValidationMessages): string {
+    for(const {type, message} of this.addTodoValidationMessages[name]) {
+      if (this.addTodoForm.get(name).hasError(type)) {
+        return message;
+      }
+    }
+    return 'Unknown error';
+  }
+
+  submitForm() {
+    const newTodo = {
+      ...this.addTodoForm.value,
+      status: this.addTodoForm.value.status === 'complete'
+    };
+    this.todoService.addTodo(newTodo).subscribe({
+      next: (newId) => {
+        this.snackBar.open(
+          `Added todo ${this.addTodoForm.value.owner}`,
+          null,
+          { duration: 2000 }
+        );
+        this.router.navigate(['/todos/', newId]);
+      },
+      error: err => {
+        if (err.status === 400) {
+          this.snackBar.open(
+            `Tried to add an illegal new todo – Error Code: ${err.status}\nMessage: ${err.message}`,
+            'OK',
+            { duration: 5000 }
+          );
+        } else if (err.status === 500) {
+          this.snackBar.open(
+            `The server failed to process your request to add a new todo. Is the server up? – Error Code: ${err.status}\nMessage: ${err.message}`,
+            'OK',
+            { duration: 5000 }
+          );
+        } else {
+          this.snackBar.open(
+            `An unexpected error occurred – Error Code: ${err.status}\nMessage: ${err.message}`,
+            'OK',
+            { duration: 5000 }
+          );
+        }
+      },
+    });
+  }
+
+}
